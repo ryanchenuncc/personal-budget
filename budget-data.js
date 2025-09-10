a9862189ff2037d822ebbcb650d43765d7dad7d9
Ryan Chen <ryanchenwry@gmail.com>
2025-09-09 22:06:21 -0400

    Loading data from JSON file

---
+++ budget-data.js
@@ -0,0 +1,34 @@
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 25
+        },
+        {
+            title: 'Rent',
+            budget: 275
+        },
+        {
+            title: 'Grocery',
+            budget: 110
+        },
+        {
+            title: 'Gym',
+            budget: 25
+        },
+        {
+            title: 'Car',
+            budget: 150
+        },
+        {
+            title: 'Phone',
+            budget: 45
+        },
+        {
+            title: 'Internet',
+            budget: 60
+        }
+    ]
+};
+
+module.exports = budget;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Budget API
-
+const budget = require('./budget-data.js');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,23 +7,6 @@ const port = 3000;
 
 // app.use(cors());
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
